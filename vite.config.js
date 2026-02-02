45539e273f8a676accd72cd4abb8de8776c50927
eshanikane <eshani.kane@somaiya.edu>
2026-02-02 12:58:01 +0530

    Added deploy workflow for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages serves this project from a subpath:
+  // https://eshanikane.github.io/Eshani-Portfolio/
+  base: '/Eshani-Portfolio/',
   plugins: [react()],
 })
